7395690f4ce3caf68ee0316e6f27f7006ef88bc4
Javas Alfreda Belva Yoga Pratama <80810063+javasalfreda@users.noreply.github.com>
2026-07-01 16:26:21 +0700

    Update etl_all_boards_cekat.py

--- etl_all_boards_cekat.py
+++ etl_all_boards_cekat.py
@@ -89,7 +89,7 @@ def extract_cekat_raw():
             all_data.extend(data)
             print(f"✅ {board_name} - Page {page} berhasil ditarik ({len(data)} rows)")
 
-            if len(data) < 1000:
+            if len(data) <= 0:
                 break
             
             page += 1
