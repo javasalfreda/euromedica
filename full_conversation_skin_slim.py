b6c1e49362f9dbb04710052417449171b36a05d6
digitaleuromedica-cyber <digitaleuromedica@gmail.com>
2026-08-22 09:31:13 +0700

    Fix chat_credits_used conversion to string
    Ensure chat_credits_used is converted to string format before saving.

--- full_conversation_skin_slim.py
+++ full_conversation_skin_slim.py
@@ -96,6 +96,7 @@ columns_to_show = [
   "ads_data"
 ]
 
+df_messages["chat_credits_used"] = df_messages["chat_credits_used"].apply(lambda x: None if pd.isna(x) else str(x))
 
 execution_date_str = today_dt.strftime('%Y%m%d')
 file_path = f"cekat_full_conversations_{execution_date_str}.parquet"
@@ -113,7 +114,7 @@ job_config = bigquery.LoadJobConfig(
     write_disposition=bigquery.WriteDisposition.WRITE_APPEND, # Create or Replace
 )
 
-df_messages["chat_credits_used"] = df_messages["chat_credits_used"].apply(lambda x: None if pd.isna(x) else str(x))
+
 
 with open(file_path, "rb") as source_file:
     job = client.load_table_from_file(source_file, PROD_TABLE_ID, job_config=job_config)
