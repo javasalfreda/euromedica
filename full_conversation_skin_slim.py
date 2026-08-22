83af966abad817363e1e80ada2309ef446c5b136
digitaleuromedica-cyber <digitaleuromedica@gmail.com>
2026-08-22 17:20:00 +0700

    Update full_conversation_skin_slim.py

--- full_conversation_skin_slim.py
+++ full_conversation_skin_slim.py
@@ -14,8 +14,8 @@ headers = {"Authorization": f"Bearer {API_KEY}"}
 
 # Atur rentang tanggal sesuai kebutuhan Anda (format: YYYY-MM-DD)
 today_dt = datetime.utcnow()
-start_date_str = "2026-07-25"
-end_date_str = "2026-07-27"
+start_date_str = "2026-07-28"
+end_date_str = "2026-07-31"
 
 all_messages = []
 current_page = 1
