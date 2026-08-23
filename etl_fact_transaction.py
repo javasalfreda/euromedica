6c2bbbd0d234f1240bd5e205d433abfec9f021bb
digitaleuromedica-cyber <digitaleuromedica@gmail.com>
2026-08-23 07:48:27 +0700

    Change date range in ETL function
    Updated date range for ETL process to reflect new year.

--- etl_fact_transaction.py
+++ etl_fact_transaction.py
@@ -17,10 +17,10 @@ REPORT_NAME = "Item-wise Sales History"
 def etl_erp_to_parquet():
     # Menggunakan datetime hari ini (karena dijalankan harian oleh cron)
     execution_date = datetime.utcnow()
-    from_date = datetime(2025, 6, 1)
-    to_date = datetime(2025, 12, 31)
+    from_date = datetime(2026, 1, 1)
+    #to_date = datetime(2026, 12, 31)
     #from_date = (execution_date - relativedelta(months=1)).replace(day=1)
-    #to_date = execution_date
+    to_date = execution_date
     
     # Di GitHub Actions, kita bisa simpan langsung di folder workspace saat ini
     file_path = f"sales_history_up_to_{to_date.strftime('%Y%m%d')}.parquet"
